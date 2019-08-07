f3496bd5281adbd7d722cb60c5bfa3989c5bd9cf
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-07 19:33:45 -0400

    added text content for the nav items

--- js/index.js
+++ js/index.js
@@ -39,4 +39,14 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+const allLinks = document.querySelectorAll('nav a');
+allLinks[0].textContent = siteContent["nav"]["nav-item-1"];
+allLinks[1].textContent = siteContent["nav"]["nav-item-2"];
+allLinks[2].textContent = siteContent["nav"]["nav-item-3"];
+allLinks[3].textContent = siteContent["nav"]["nav-item-4"];
+allLinks[4].textContent = siteContent["nav"]["nav-item-5"];
+allLinks[5].textContent = siteContent["nav"]["nav-item-6"];
+console.log(allLinks);
+
