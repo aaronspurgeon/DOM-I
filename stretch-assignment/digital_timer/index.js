7060cee252d8911a984877bd16bae909183124f2
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-07 21:05:48 -0400

    started stretch project

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,15 @@
+const msTens = document.getElementById('msTens');
+const msHundreds = document.getElementById('msHundreds');
+const seconds = document.getElementById('secondOnes');
+const tens = document.getElementById('secondTens');
+
+setInterval(() => {
+    msTens.innerText = 1;
+    for (let i = 0; i < msTens.innerText; i++) {
+        msTens.innerText++;
+        if (msTens.innerText === 10) {
+            msTens.innerText = 1;
+            msHundreds.innerText = 1;
+        }
+    }
+}, 10);
